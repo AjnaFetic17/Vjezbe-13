374814c704c2bf37620f3bcd29d6331b91531511
Ajna17 <ajna.fetic@blandus.ba>
2022-01-18 15:57:41 +0100

    UserAdded

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { rmSync } = require("fs");
 
 const app = express();
 
@@ -9,49 +10,231 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //koristi public folder za spremanje static fajlova
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/novostiDB");
+mongoose.connect("mongodb://localhost:27017/proizvodiDB");
 
-const articleSchema = {
+const productSchema = {
     title: String,
-    content: String
+    content: String,
+    imageURL: String,
+    price: Number
 }
 
-const Article = mongoose.model("Article", articleSchema);
+const Product = mongoose.model("Product", productSchema);
 
-app.get("/articles", (req, res) => {
-    Article.find((err, foundArticles) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(foundArticles);
-        }
+//PRVO OVO
+
+// app.get("/products", (req, res) => {
+//     Product.find((err, foundProducts) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.send(foundProducts);
+//         }
+//     })
+// });
+
+// app.post("/products", (req, res) => {
+//     const newProduct = new Product({
+//         title: req.body.title,
+//         content: req.body.content,
+//         imageURL: req.body.imageURL,
+//         price: parseFloat(req.body.price)
+//     });
+//     newProduct.save((err) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.send("Successfully added a new article.");
+//         }
+//     });
+// });
+
+// app.delete("/products", (req, res) => {
+//     Product.deleteMany((err) => {
+//         if (err) {
+//             res.send("Successfully deleted all products.")
+//         } else {
+//             res.send(err)
+//         }
+//     });
+// });
+
+//PA OVO
+app.route("/products")
+    .get((req, res) => {
+        Product.find((err, foundProducts) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(foundProducts);
+            }
+        });
     })
-});
-
-app.post("/articles", (req, res) => {
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+    .post((req, res) => {
+        const newProduct = new Product({
+            title: req.body.title,
+            content: req.body.content,
+            imageURL: req.body.imageURL,
+            price: parseFloat(req.body.price)
+        });
+        newProduct.save((err) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("New product successfully added.")
+            }
+        });
+    })
+    .delete((req, res) => {
+        Product.deleteMany((err) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("All products successfully deleted.")
+            }
+        })
     });
-    newArticle.save((err) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send("Successfully added a new article.");
-        }
-    });
-});
 
-app.delete("/articles", (req, res) => {
-    Article.deleteMany((err) => {
-        if (err) {
-            res.send("Successfully deleted all articles.")
-        } else {
-            res.send(err)
-        }
+//SINGLE ARTICLE
+app.route("/products/:articleTitle")
+    .get((req, res) => {
+        Product.findOne({ title: req.params.articleTitle }, (err, foundProduct) => {
+            if (foundProduct) {
+                res.send(foundProduct);
+            } else {
+                res.send("No products found");
+            }
+        });
+    })
+    .put((req, res) => {
+        Product.findOneAndUpdate(
+            { title: req.params.articleTitle },
+            { title: req.body.title, content: req.body.content, imageURL: req.body.imageURL, price: parseFloat(req.body.price) },
+            { overwrite: true },
+            (err) => {
+                if (!err) {
+                    res.send("Successfully updated article.")
+                } else {
+                    res.send("No article was updated");
+                }
+            }
+        );
+    })
+    .patch((req, res) => {
+        Product.findOneAndUpdate(
+            { title: req.params.articleTitle },
+            { $set: req.body },
+            (err) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("Successful patch")
+                }
+            }
+        )
+    })
+    .delete((req, res) => {
+        Product.deleteOne(
+            { title: req.params.articleTitle },
+            (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.send("Successful delete");
+                }
+            }
+        )
     });
-});
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+const userSchema = {
+    username: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema)
+
+app.route("/users")
+    .get((req, res) => {
+        User.find((err, foundUsers) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(foundUsers);
+            }
+        })
+    })
+    .post((req, res) => {
+        const newUser = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        User.find({ username: req.body.username }, (err, foundUsers) => {
+            if (err) {
+                res.send(err);
+            } else {
+                if (foundUsers.length === 0) {
+                    newUser.save((err) => {
+                        if (err) {
+                            res.send(err);
+                        } else {
+                            res.send("User added successfully")
+                        }
+                    })
+                } else {
+                    res.send("User with this username already exists");
+                }
+            }
+        })
+    })
+    .delete((req, res) => {
+        User.deleteMany((err) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("All users have been deleted successfully.");
+            }
+        })
+    });
+
+app.route("/users/:username")
+    .get((req, res) => {
+        User.find({ username: req.params.username }, (err, foundUser) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(foundUser);
+            }
+        })
+    })
+    .put((req, res) => {
+        User.findOneAndUpdate(
+            { username: req.params.username },
+            { username: req.body.username, password: req.body.password },
+            { overwrite: true },
+            (err) => {
+                if (!err) {
+                    res.send("Successfully updated user.")
+                } else {
+                    res.send("No user was updated");
+                }
+            }
+        )
+    })
+    .patch((req, res) => {
+        User.findOneAndUpdate(
+            { username: req.params.username },
+            { $set: req.body },
+            (err) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("Successful patch")
+                }
+            }
+        )
+    });
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
 });
