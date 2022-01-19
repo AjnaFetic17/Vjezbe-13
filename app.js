8a6a4029b915ca3d8afe58e51126eb3f972d990e
Ajna17 <ajna.fetic@blandus.ba>
2022-01-19 13:50:47 +0100

    cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //koristi public folder za spremanje static fajlova
 app.use(express.static("public"));
